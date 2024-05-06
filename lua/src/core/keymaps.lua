febbdaddaa798df53f52728945244a020df3d154
Lucas Deprit <ldeprit_deloitte@bankinter.com>
2024-05-06 12:38:06 +0200

    update in window management keymaps

--- lua/src/core/keymaps.lua
+++ lua/src/core/keymaps.lua
@@ -14,12 +14,6 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
@@ -28,3 +22,14 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- splits management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+
+-- resize with arrows
+vim.keymap.set("n", "<down>", "<cmd>resize -4<cr>", { desc = "Smaller horizontal split" })
+vim.keymap.set("n", "<up>", "<cmd>resize +4<cr>", { desc = "Bigger horizontal split" })
+vim.keymap.set("n", "<left>", "<cmd>vertical resize -4<cr>", { desc = "Smaller vertical split" })
+vim.keymap.set("n", "<right>", "<cmd>vertical resize +4<cr>", { desc = "Bigger vertical split" })
