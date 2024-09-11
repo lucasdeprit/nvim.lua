9a019076107e1d662c719b74a9ca0533fa9ae453
lucasdeprit <lucasdeprit@gmail.com>
2024-09-11 17:52:42 +0200

    fix in which-key keymaps

--- lua/src/plugins/wich-key.lua
+++ lua/src/plugins/wich-key.lua
@@ -2,21 +2,38 @@ return { -- Useful plugin to show you pending keybinds.
 	"folke/which-key.nvim",
 	event = "VimEnter", -- Sets the loading event to 'VimEnter'
 	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+		local wk = require("which-key")
+		wk.setup()
 
 		-- Document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+		wk.add({
+			{ "<leader>c", desc = "[C]ode", hidden = true },
+			{ "<leader>d", desc = "[D]ocument", hidden = true },
+			{ "<leader>r", desc = "[R]ename", hidden = true },
+			{ "<leader>s", desc = "[S]earch", hidden = true },
+			{ "<leader>w", desc = "[W]orkspace", hidden = true },
+			{ "<leader>t", desc = "[T]oggle", hidden = true },
+			{ "<leader>h", desc = "Git [H]unk", hidden = true },
 		})
 		-- visual mode
-		require("which-key").register({
-			["<leader>h"] = { "Git [H]unk" },
+		wk.add({
+			{ "<leader>h", desc = "Git [H]unk" },
 		}, { mode = "v" })
 	end,
 }
+--- {
+---   { "<leader>c", group = "[C]ode" },
+---   { "<leader>c_", hidden = true },
+---   { "<leader>d", group = "[D]ocument" },
+---   { "<leader>d_", hidden = true },
+---   { "<leader>h", group = "Git [H]unk" },
+---   { "<leader>h_", hidden = true },
+---   { "<leader>r", group = "[R]ename" },
+---   { "<leader>r_", hidden = true },
+---   { "<leader>s", group = "[S]earch" },
+---   { "<leader>s_", hidden = true },
+---   { "<leader>t", group = "[T]oggle" },
+---   { "<leader>t_", hidden = true },
+---   { "<leader>w", group = "[W]orkspace" },
+---   { "<leader>w_", hidden = true },
+--- }
